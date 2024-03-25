ed62e07518ab5b5dbbb0d587a40be9cbf61e54d9
Pabmoy25 <pabmoy1725@gmail.com>
2024-03-24 22:16:48 -0300

    se crea el Schema de una receta

---
+++ src/models/receta.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+
+const recetaSchema = new Schema({
+  nombreReceta: {
+    type: String,
+    required: true,
+    minLengh: 2,
+    MaxLength: 50,
+    unique: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    minLengh: 10,
+    MaxLength: 60,
+  },
+  imagen: {
+    type: String,
+    required: true,
+    validate: (valor) => {
+      /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg)/.test(valor);
+    },
+    message: (dato) => `${dato.value} no es una URL de imagen valida`,
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: ["Postres y Dulces", "Pastas", "Carnes", "Guarniciones", "Minutas"],
+  },
+  ingredientes: {
+    type: String,
+    required: true,
+    minLengh: 20,
+    MaxLength: 500,
+  },
+  preparacion: {
+    type: String,
+    required: true,
+    minLengh: 50,
+    MaxLength: 1000,
+  },
+});
