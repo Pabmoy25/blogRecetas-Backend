746ad9d48573d07b6b461c3e34eb4a422f96582f
Pabmoy25 <pabmoy1725@gmail.com>
2024-03-24 23:01:48 -0300

    se agrega la funcion para crear un producto

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -1,3 +1,16 @@
+import Receta from "../models/receta.js"
+
 export const listaRecetas = (req,res)=>{
     res.send('enviando ')
     }
+
+export const crearReceta = async (req, res)=>{
+try {
+    const recetaNueva = new Receta(req.body)
+    await recetaNueva.save()
+    res.status(201).json({mensaje:'La receta fue creada exitosamente'})
+} catch (error) {
+    console.log(error)
+    res.status(400).json({mensaje:'La receta no pudo ser creada'})
+}
+}
--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { listaRecetas } from "../controllers/recetas.controllers.js";
+import { crearReceta, listaRecetas } from "../controllers/recetas.controllers.js";
 
 const router = Router();
 
-router.route('/recetas').get(listaRecetas)
+router.route('/recetas').get(listaRecetas).post(crearReceta)
+
 
 export default router
