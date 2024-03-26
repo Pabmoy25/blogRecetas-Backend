eccb310c67bc7433eb6ad4966a114900c23760c5
Pabmoy25 <pabmoy1725@gmail.com>
2024-03-26 11:58:16 -0300

    se trabaja en la logica para crear usuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -10,3 +10,14 @@ export const listarUsuarios = async (req, res) => {
     res.status(500).json({ mensaje: "Error al buscar el usuario" });
   }
 };
+
+export const crearUsuario = async (req, res) => {
+  try {
+    const usuarioNuevo = new Usuario(req.body);
+    await usuarioNuevo.save();
+    res.status(201).json({ mensaje: "El usuario fue creado exitosamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ mensaje: "El usuario no pudo ser creado" });
+  }
+};
